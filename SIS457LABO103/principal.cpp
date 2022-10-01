dcbe2e03a50eaef553459f9cf9d42399cdb4b601
Luis Ibarra <ibarra.luisalberto23@gmail.com>
2022-10-01 19:28:27 -0400

    cambio

---
+++ SIS457LABO103/principal.cpp
@@ -0,0 +1,14 @@
+/*Nombre: Luis Alberto Ibarra Calderon
+C.I:7467171 Ch.
+C.U: 35-4620
+Correo: ibarra.luisalberto23@gmail.com*/
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "hola mundo\n";
+	cout << "Nombre: Luis Alberto Ibarra Calderon\n" <<
+		"C.I: 7467171 Ch.\n" <<
+		"C.U: 35-4620\n" <<
+		"Correo: ibarra.luisalberto23@gmail.com\n";
+	return 0;
+}
